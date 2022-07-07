9451898f8086a9a519d9f75d0413d8cb8456345b
Thaparoshan1477 <thaparoshan629@gmail.com>
2022-07-07 16:28:23 -0700

    fic problem

---
+++ src/main/java/com/example/newSpringBootProject/NewSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.newSpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newSpringBootProject/NewSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
